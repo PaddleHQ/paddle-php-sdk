a6faf65883635662f0cb633219f67ed30c5f424b
Víctor Fernández Saborit <victorfs.sp@gmail.com>
2024-02-15 11:47:39 +0100

    Update resume subscription options (#36)

---
+++ src/Entities/Subscription/SubscriptionResumeEffectiveFrom.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * |------
+ * | ! Generated code !
+ * | Altering this code will result in changes being overwritten |
+ * |-------------------------------------------------------------|.
+ */
+
+namespace Paddle\SDK\Entities\Subscription;
+
+use Paddle\SDK\PaddleEnum;
+
+/**
+ * @method static SubscriptionResumeEffectiveFrom Immediately()
+ */
+class SubscriptionResumeEffectiveFrom extends PaddleEnum
+{
+    private const Immediately = 'immediately';
+}
