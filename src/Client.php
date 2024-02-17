33dd75e67f646c241ffec15ffc95ab5d6b0ff2af
Michael Woodward <michael.woodward@paddle.com>
2024-02-17 00:02:20 +0000

    docs(release): 1.0.0

--- src/Client.php
+++ src/Client.php
@@ -51,7 +51,7 @@ use Symfony\Component\Uid\Ulid;
 
 class Client
 {
-    private const SDK_VERSION = '0.3.0';
+    private const SDK_VERSION = '1.0.0';
 
     public readonly LoggerInterface $logger;
     public readonly Options $options;
