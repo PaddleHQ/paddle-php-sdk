a66ff3201208340441dcc02c141057362b375831
David Grayston <david.grayston@paddle.com>
2024-09-12 14:32:48 +0100

    Feature: Support disposition query parameter

--- src/Entities/Shared/Disposition.php
+++ src/Entities/Shared/Disposition.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/**
+ * |------
+ * | ! Generated code !
+ * | Altering this code will result in changes being overwritten |
+ * |-------------------------------------------------------------|.
+ */
+
 namespace Paddle\SDK\Entities\Shared;
 
 use Paddle\SDK\PaddleEnum;
