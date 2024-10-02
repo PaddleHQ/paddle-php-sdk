eab1827799783dfe37bed9bf0677897ba5aa46f8
Victor Fernandez Saborit <victorfs.sp@gmail.com>
2024-10-02 15:45:57 +0200

    feat: Adding simulation types

--- src/Client.php
+++ src/Client.php
@@ -33,6 +33,7 @@ use Paddle\SDK\Resources\PricingPreviews\PricingPreviewsClient;
 use Paddle\SDK\Resources\Products\ProductsClient;
 use Paddle\SDK\Resources\Reports\ReportsClient;
 use Paddle\SDK\Resources\Simulations\SimulationsClient;
+use Paddle\SDK\Resources\SimulationTypes\SimulationTypesClient;
 use Paddle\SDK\Resources\Subscriptions\SubscriptionsClient;
 use Paddle\SDK\Resources\Transactions\TransactionsClient;
 use Psr\Http\Message\RequestFactoryInterface;
@@ -74,6 +75,7 @@ class Client
     public readonly NotificationLogsClient $notificationLogs;
     public readonly ReportsClient $reports;
     public readonly SimulationsClient $simulations;
+    public readonly SimulationTypesClient $simulationTypes;
 
     private readonly HttpAsyncClient $httpClient;
     private readonly RequestFactoryInterface $requestFactory;
@@ -118,6 +120,7 @@ class Client
         $this->notificationLogs = new NotificationLogsClient($this);
         $this->reports = new ReportsClient($this);
         $this->simulations = new SimulationsClient($this);
+        $this->simulationTypes = new SimulationTypesClient($this);
     }
 
     public function getRaw(string|UriInterface $uri, array|HasParameters $parameters = []): ResponseInterface
