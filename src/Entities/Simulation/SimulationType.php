f1efdec731ac59f0f9b35c95f7da7ee3f230540c
Victor Fernandez Saborit <victorfs.sp@gmail.com>
2024-10-02 15:45:57 +0200

    feat: Adding simulation types

---
+++ src/Entities/Simulation/SimulationType.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * |------
+ * | ! Generated code !
+ * | Altering this code will result in changes being overwritten |
+ * |-------------------------------------------------------------|.
+ */
+
+namespace Paddle\SDK\Entities\Simulation;
+
+use Paddle\SDK\PaddleEnum;
+
+/**
+ * @method static SimulationType Scenario()
+ * @method static SimulationType SingleEvent()
+ */
+final class SimulationType extends PaddleEnum
+{
+    private const Scenario = 'scenario';
+    private const SingleEvent = 'single_event';
+}
