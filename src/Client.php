622451be9087bf19fb9645e024576ac02895d053
davidgrayston-paddle <david.grayston@paddle.com>
2025-01-30 17:09:55 +0000

    docs: Release 1.9.0 (#114)

--- src/Client.php
+++ src/Client.php
@@ -60,7 +60,7 @@ use Symfony\Component\Uid\Ulid;
 
 class Client
 {
-    private const SDK_VERSION = '1.8.0';
+    private const SDK_VERSION = '1.9.0';
 
     public readonly LoggerInterface $logger;
     public readonly Options $options;
