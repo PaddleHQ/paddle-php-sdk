7957f82ace81b90766f46a96bff61ca9119fa414
David Grayston <david.grayston@paddle.com>
2025-09-12 17:19:34 +0100

    docs(release): v1.11.0

--- src/Client.php
+++ src/Client.php
@@ -53,7 +53,7 @@ use Symfony\Component\Uid\Ulid;
 
 class Client
 {
-    private const SDK_VERSION = '1.10.0';
+    private const SDK_VERSION = '1.11.0';
 
     public readonly LoggerInterface $logger;
     public readonly Options $options;
